7e6c8a2ea483bee97801f59f5d6122336b86d28c
Abulkhair <abzh423@protonmail.com>
2022-03-04 09:22:52 +0600

    Logo change 32x32

--- frontend/src/partials/Header.jsx
+++ frontend/src/partials/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Transition from "../utils/Transition";
+import LOGO from "../images/logo.png";
 
 function Header() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
@@ -56,33 +57,7 @@ function Header() {
           <div className="shrink-0 mr-4">
             {/* Logo */}
             <Link to="/" className="block" aria-label="Cruip">
-              <svg
-                className="w-8 h-8"
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <defs>
-                  <radialGradient
-                    cx="21.152%"
-                    cy="86.063%"
-                    fx="21.152%"
-                    fy="86.063%"
-                    r="79.941%"
-                    id="header-logo"
-                  >
-                    <stop stopColor="#4FD1C5" offset="0%" />
-                    <stop stopColor="#81E6D9" offset="25.871%" />
-                    <stop stopColor="#338CF5" offset="100%" />
-                  </radialGradient>
-                </defs>
-                <rect
-                  width="32"
-                  height="32"
-                  rx="16"
-                  fill="url(#header-logo)"
-                  fillRule="nonzero"
-                />
-              </svg>
+              <img src={LOGO} alt="" style={{ height: 42, width: 42 }} />
             </Link>
           </div>
 
