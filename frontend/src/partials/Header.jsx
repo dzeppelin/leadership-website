23d9cc8217383f07a0c2e6a9b6a2e5cf8681af5a
Abulkhair <abzh423@protonmail.com>
2022-03-06 13:58:54 +0600

    Logo change

--- frontend/src/partials/Header.jsx
+++ frontend/src/partials/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Transition from "../utils/Transition";
-import LOGO from "../images/logotype.png";
+import LOGO from "../images/favicon.png";
 
 function Header() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
