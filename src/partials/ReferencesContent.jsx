947c77198dd8aac50fab9f9b97aebee4ba3ccf0b
Abulkhair <abzh423@protonmail.com>
2022-03-12 14:48:48 +0600

    Change in references

--- src/partials/ReferencesContent.jsx
+++ src/partials/ReferencesContent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import DocsImage01 from "../images/docs-image-01.png";
+import fifty from "../images/50.jpg";
+import fastlane from "../images/fastlane.jpg";
 
 function DocumentationContent() {
   return (
@@ -8,52 +9,59 @@ function DocumentationContent() {
       <div className="text-lg text-gray-600">
         <h2 className="h2 text-gray-900 mb-4">Books we would recommend</h2>
         <p className="mb-8">That is a list of books that we would recommend</p>
-        <h2
-          id="installation"
-          className="h3 text-gray-900 mb-4"
-          style={{ scrollMarginTop: "100px" }}
-        >
-          50th law
-        </h2>
-        <p className="mb-8">
-          Sit amet cursus sit amet dictum sit amet. Mattis pellentesque id nibh
-          tortor id nulla aliquet porttitor lacus luctus accumsan, tortor
-          posuere pellentesque habitant morbi tristique senectus et netus.
-          Lacinia quis vel eros donec ac odio tempor duis ut diam quam nulla.
-          Tellus orci ac auctor augue mauris augue.
-        </p>
-        <figure className="mb-8">
-          <img
-            className="w-full rounded"
-            src={DocsImage01}
-            width="768"
-            height="390"
-            alt="Docs 01"
-          />
-        </figure>
-        <h2
-          id="installation"
-          className="h3 text-gray-900 mb-4"
-          style={{ scrollMarginTop: "100px" }}
-        >
-          Installation
-        </h2>
-        <p className="mb-8">
-          Sit amet cursus sit amet dictum sit amet. Mattis pellentesque id nibh
-          tortor id nulla aliquet porttitor lacus luctus accumsan, tortor
-          posuere pellentesque habitant morbi tristique senectus et netus.
-          Lacinia quis vel eros donec ac odio tempor duis ut diam quam nulla.
-          Tellus orci ac auctor augue mauris augue.
-        </p>
-        <figure className="mb-8">
-          <img
-            className="w-full rounded"
-            src={DocsImage01}
-            width="768"
-            height="390"
-            alt="Docs 01"
-          />
-        </figure>
+
+        <div className="flex flex-col">
+          <div className="w-3/4">
+            <h2
+              id="installation"
+              className="h3 text-gray-900 mb-4"
+              style={{ scrollMarginTop: "100px" }}
+            >
+              The 50th law
+            </h2>
+            <p className="mb-8">
+              There are no Alps and no obstacles that can stand in the way of a
+              person without fears.
+            </p>
+          </div>
+          <figure className="mb-8 ml-5 w-1/4">
+            <div className="">
+              <img
+                className="w-full rounded"
+                src={fifty}
+                alt="The 50th law"
+                height={180}
+              />
+            </div>
+          </figure>
+        </div>
+
+        <div className="flex flex-col">
+          <div className="w-3/4">
+            <h2
+              id="installation"
+              className="h3 text-gray-900 mb-4"
+              style={{ scrollMarginTop: "100px" }}
+            >
+              The Millionaire Fastlane
+            </h2>
+            <p className="mb-8">
+              The Millionaire Fastlane is about getting wealthy and retiring in
+              10 years rather than 40 years. <br />
+              <i>Requires specific mindset</i>
+            </p>
+          </div>
+          <figure className="mb-8 ml-5 w-1/4">
+            <div className="">
+              <img
+                className="w-full rounded"
+                src={fastlane}
+                height={180}
+                alt="The Millionaire Fastlane"
+              />
+            </div>
+          </figure>
+        </div>
       </div>
     </div>
   );
